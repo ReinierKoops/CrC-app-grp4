22628648b290b5f1c9a4d08481a5a36148fa5a1f
ReinierKoops <info@reinier.work>
2020-06-02 15:26:42 +0200

    added vuex components.

---
+++ src/store/modules/auth.js
@@ -0,0 +1,25 @@
+const state = {
+    username: null,
+    user_id: null,
+    email: null,
+}
+
+const mutations = {
+
+}
+
+const actions = {
+  
+}
+
+const getters = {
+  
+}
+
+
+export default {
+    state,
+    getters,
+    actions,
+    mutations
+};
