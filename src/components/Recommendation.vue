409880bc79a58e6f22f675a5d38ed56ac9eb961e
wturk <w.r.a.turk@student.tudelft.nl>
2020-06-02 11:19:38 +0200

    Change preference list and recommendation

--- src/components/Recommendation.vue
+++ src/components/Recommendation.vue
@@ -1,9 +1,11 @@
 <template>
   <div>
     <h3>Recommendations</h3>  
-    <ul>
-      <li :draggable="draggable" v-for="song in songs" :id="song" :key="song" v-on:dragstart="dragStart" v-on:dragenter="dragEnter" v-on:drop="dragDrop" v-on:dragover="dragOver">{{ song }}</li>
-    </ul>    
+    <v-list>
+      <v-list-item :draggable="draggable" v-for="song in songs" :key="song" v-on:dragstart="dragStart" v-on:dragenter="dragEnter" v-on:drop="dragDrop" v-on:dragover="dragOver">
+        <v-list-item-content :id="song"><v-list-item-title v-text="song"/></v-list-item-content>
+      </v-list-item>
+    </v-list>    
   </div>
 </template>
 
