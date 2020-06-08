c4f03047f9ac23d6c69fa6c7dfa97af4ef2432a2
ReinierKoops <info@reinier.work>
2020-06-08 11:13:03 +0200

    Updated styling Fix & Verify

--- src/views/Verify.vue
+++ src/views/Verify.vue
@@ -1,63 +1,193 @@
 <template>
-    <div class="verify">
-        <v-dialog v-model="dialog" persistent width="500" height="300">
+    <v-container 
+    fluid
+    class="fill-height verify"
+    >
+        <v-dialog 
+        v-model="dialog" 
+        persistent 
+        width="500" 
+        height="300">
             <v-card>
-                <v-card-title class="headline">We could not find a task for you!</v-card-title>
+                <v-card-title 
+                class="headline">
+                    We could not find a task for you!
+                </v-card-title>
 
                 <v-card-text>
-                While fetching a task for you something went wrong! It could be the case that you completed more than 5 tasks of this type or that no task is available at this time. Please try again later!
+                    While fetching a task for you something went wrong! It could be the case that you completed more than 5 tasks of this type or that no task is available at this time. Please try again later!
                 </v-card-text>
 
                 <v-divider></v-divider>
 
                 <v-card-actions>
                 <v-spacer></v-spacer>
-                <v-btn :block="true" color="#2296F3" class="white--text" :to="{ name: 'Home' }">Return to the homepage</v-btn>
+
+                <v-btn 
+                :block="true" 
+                color="#2296F3" 
+                class="white--text" 
+                :to="{ name: 'Home' }">
+                    Return to the homepage
+                </v-btn>
+
                 </v-card-actions>
             </v-card>
         </v-dialog>
-        <v-progress-circular id="loader" indeterminate size="70" color="primary"></v-progress-circular>
-        <v-container id="task">
+
+        <v-progress-circular 
+        id="loader" 
+        indeterminate 
+        size="70" 
+        color="primary">
+        </v-progress-circular>
+
+        <v-col
+        cols="1"
+        >
+        </v-col>
+
+        <v-col
+        cols="10"
+        id="task">
+
             <v-row>
-                <v-col cols="3">
-                    <recommendation name="New Recommendation" :songs="task.fix"></recommendation>
-                    <recommendation name="Old Recommendation" :songs="task.algorithm"></recommendation>   
+
+                <v-col 
+                cols="3"
+                class="add-some-padding">
+
+
+                        <recommendation 
+                        name="New Recommendation" 
+                        subname="by other users."
+                        :songs="task.fix"
+                        class="add_margin"
+                        />
+
+                        <recommendation 
+                        name="Old Recommendation" 
+                        subname="by algorithm."
+                        :songs="task.algorithm"
+                        />   
+
+
                 </v-col>
-                <v-col>
+
+                <v-col
+                cols="9">
+
                     <v-row>
-                        <v-col v-for="(prefs, index) in task.preferences" :key="index">
-                            <preference-list :songs="prefs" :name="'User ' + (index + 1)"></preference-list>
+                        <v-col 
+                        v-for="(prefs, index) in task.preferences" 
+                        :key="index"
+                        >
+                            <preference-list 
+                            :songs="prefs" 
+                            name="Preference top 5"
+                            :subname="'by User ' + (index + 1) + '.'"
+                            />
                         </v-col>
                     </v-row>
+
                     <v-row>
-                        <v-col>
-                            <b>Are the <i>new</i> recommendations fair to you?</b>
-                            <v-radio-group>
-                                <v-radio name="fair" label="Yes" :value="true"/>
-                                <v-radio name="fair" label="No" :value="false"/>
-                            </v-radio-group>
-                            <v-text-field id="rationale" label="Explanation" outlined/>
-                            <transition name="fade">
-                                <v-alert v-if="show" type="error">
-                                    {{ errorText }}
-                                </v-alert>
-                            </transition>
-                            <v-btn v-on:click="clickSubmit">Submit</v-btn>
+                        <v-col
+                        cols="6">
+                            <v-card 
+                            class="elevation-10"
+                            tile
+                            >
+                                <v-card-title class="subtitle-1">
+                                    Is the <b>"New Recommendations"</b> fair to you?
+                                </v-card-title>
+
+                                <v-divider></v-divider>
+
+                                <v-card-text>
+                                    <v-radio-group>
+                                        <v-radio 
+                                        name="fair" 
+                                        label="Yes" 
+                                        :value="true"/>
+                                        <v-radio 
+                                        name="fair" 
+                                        label="No" 
+                                        :value="false"/>
+                                    </v-radio-group>
+
+                                    <v-text-field 
+                                    id="rationale" 
+                                    clearable
+                                    dense
+                                    :counter="100"
+                                    label="Explanation" 
+                                    outlined/>
+
+                                    <transition 
+                                    name="fade">
+                                        <v-alert 
+                                        v-if="show" 
+                                        type="error">
+                                            {{ errorText }}
+                                        </v-alert>
+                                    </transition>
+
+                                    <v-btn 
+                                    tile 
+                                    :block="true"
+                                    color="#2296F3"
+                                    class="white--text"
+                                    v-on:click="clickSubmit">
+                                        <v-icon>mdi-send</v-icon>Submit
+                                    </v-btn>
+                                </v-card-text>
+
+                            </v-card>
                         </v-col>
-                        <v-col>
-                            <b>Explanations on why the recommendation <i>was</i> not fair:</b>
-                            <v-list disabled>
-                                <v-list-item v-for="(rationale, index) in task.explanations" :key="index">
-                                    <v-list-item-icon><v-icon v-text="icon"/></v-list-item-icon>
-                                    <v-list-item-content><v-list-item-title v-text="rationale"/></v-list-item-content>
-                                </v-list-item>
-                            </v-list>
+
+                        <v-col
+                        cols="6">
+                            <v-card 
+                            class="elevation-10"
+                            tile
+                            >
+                                <v-card-title class="subtitle-1">
+                                    Explanations <b>"Old Recommendation"</b> unfair:
+                                </v-card-title>
+
+                                <v-divider></v-divider>
+
+                                <v-list disabled>
+                                    <v-list-item 
+                                    v-for="(rationale, index) in task.explanations" 
+                                    :key="index">
+                                        
+                                        <v-list-item-icon>
+                                            <v-icon>mdi-account-voice</v-icon>
+                                        </v-list-item-icon>
+
+                                        <v-list-item-content>
+                                            <v-list-item-text 
+                                            v-text="rationale"
+                                            class="caption"/>
+                                        </v-list-item-content>
+
+                                    </v-list-item>
+                                </v-list>
+                            </v-card>
                         </v-col>
                     </v-row>
+
                 </v-col>
             </v-row>
-        </v-container>
-    </div>
+        </v-col>
+
+        <v-col
+        cols="1"
+        >
+        </v-col>
+
+    </v-container>
 </template>
 
 <script>
@@ -167,6 +297,12 @@ export default {
 </script>
 
 <style>
+.add-some-padding{
+    padding-top: 24px;
+}
+.add_margin{
+    margin-bottom: 24px;
+}
 .fade-enter-active, .fade-leave-active {
   transition: opacity .5s;
 }
